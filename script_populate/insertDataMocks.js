7aeae2aa6607bf3cb806b1c0148b8ee1fc1338b7
Darlison <moisesdarlison91@gmail.com>
2022-11-08 10:00:35 -0300

    feat: create dinamic builders object to txt/csv file

--- script_populate/insertDataMocks.js
+++ script_populate/insertDataMocks.js
@@ -5,23 +5,40 @@ function makeId(length) {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
+    for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
 }
 
-function buildUserData(person) {
-    const newPerson = Object.values(person)
+function buildUserData(person, separateWords = ';') {
+    let buildPerson = ''
+    Object.values(person).forEach(element => {
+        if (typeof element !== 'object') {
+            buildPerson += element + separateWords
+        } else {
+            buildPerson += buildUserData(element, separateWords)
+        }
+    });
+    return buildPerson
+}
 
-    newPerson.splice(11, 1)
-    newPerson.push(Object.values(person[11]))
+function getBuildedHeader(person, separateWords = ';') {
+    let buildHeader = ''
+    for (const [key, value] of Object.entries(person)) {
+        if (typeof value !== 'object') {
+            buildHeader += key + separateWords
+        } else {
+            buildHeader += getBuildedHeader(value, separateWords)
+        }
+    }
 
-    return newPerson.toString().replaceAll(',', ';')
+    return buildHeader
 }
 
 
-function getDataClient() {
+
+function getDataClient(idCount = 'x') {
     const defaultName = makeId(4) + makeId(5) + makeId(5)
     const person = {
         nome: defaultName,
@@ -29,7 +46,7 @@ function getDataClient() {
         pai: defaultName + makeId(5),
         site: "http://" + makeId(19) + ".com.br",
         email: defaultName + "@" + makeId(4) + ".com.br",
-        senha: "aQ4&!P#k4",
+        idCount: idCount,
         rg: "BR-" + Math.floor(Math.random() * 20),
         cpf: Math.floor(Math.random() * 999) + "." + Math.floor(Math.random() * 999) + "." + Math.floor(Math.random() * 999) + "-" + Math.floor(Math.random() * 99),
         telefone: "(" + Math.floor(Math.random() * 99) + ") 9" + Math.floor(Math.random() * 9999) + "-" + Math.floor(Math.random() * 9999),
@@ -50,35 +67,29 @@ function getDataClient() {
         peso: Math.floor(Math.random() * 200)
     }
 
-    return buildUserData(person)
+    return person
 }
 
-function getHeader() {
-    return 'nome;mae;pai;site;email;senha;rg;cpf;telefone;celular;dataNascimento;usuario;altura;peso;cep;logradouro;complemento;numero;bairro;cidade;estado;estadoSigla;'
-}
 
 async function main() {
     const pathConcat = path.join(__dirname, '..', 'files', 'data.txt')
     try {
-        fs.appendFile(pathConcat, getHeader() + '\n', (err) => {
+        fs.appendFileSync(pathConcat, getBuildedHeader(getDataClient(), '|') + '\n', (err) => {
             if (err) throw err;
         });
-        for (let index = 0; index < 150000; index++) {
-            fs.appendFileSync(pathConcat, getDataClient() + '\n', (err) => {
+        for (let index = 0; index < 230000; index++) {
+            fs.appendFileSync(pathConcat, buildUserData(getDataClient(index), '|') + '\n', (err) => {
                 if (err) throw err;
             });
             if (index % 500 == 0) {
                 console.log(index)
             }
         }
+        console.log("Finished", index)
     } catch (error) {
         console.log(error.message);
     }
 
-    const data = fs.readFileSync(pathConcat, { encoding: 'utf8', flag: 'r' });
-
-
-    console.log(data);
 }
 
 main()
